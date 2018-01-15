97f6a2975cb1b9aa0a97d82568b208b17adb27dd
caamanod <caamanod@RTCMW1006M>
2018-01-15 17:09:42 +0100

    Clean code

--- UserInterface/MainWindow.xaml.cs
+++ UserInterface/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using YoutubeExtractor;
 using YouTubeDownloader.Infrastructure;
 using Downloader = YoutubeExtractor.Downloader;
@@ -11,12 +12,14 @@ namespace UserInterface
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// TODO: IMPLEMENT PROGRESS BAR.
     /// </summary>
     public partial class MainWindow : Window
     {
         private int SelectedVideoIndex;
         private string YoutubeUrl;
         private List<VideoInfo> VideoWithQualities;
+        private SortedDictionary<VideoInfo, ProgressBar> VideosInListView;
         private IDownloader Downloader;
 
         public MainWindow()
@@ -24,10 +27,10 @@ namespace UserInterface
             InitializeComponent();
             YoutubeUrl = String.Empty;
             VideoWithQualities = new List<VideoInfo>();
+            VideosInListView = new SortedDictionary<VideoInfo, ProgressBar>();
             Downloader = new MyYoutubeDownloader();
         }
 
-        /// TODO: Upgrade this method to be Async and check the actual combo box items to readd them.
         private async void PopulateQuialitySelectorComboBox()
         {
             YoutubeUrl = tbYoutubeUrlToDownload.Text;
@@ -37,20 +40,23 @@ namespace UserInterface
 
             VideoWithQualities = await Task.Run(() => Downloader.RetrieveDownloadOptionsAsync(this.YoutubeUrl).Result.ToList());
 
+            //VideosInListView = await Task.Run(() => Downloader.RetrieveDownloadOptionsAsync(this.YoutubeUrl).Result.ToList());
+
             CbVideoQualities.Items.Clear();
             CbVideoQualities.SelectedValue = null;
 
             foreach (var actualItem in VideoWithQualities)
             {
                 CbVideoQualities.Items.Add(actualItem);
+                VideosInListView.Add(actualItem, new ProgressBar());
             }
         }
 
-        /// TODO: Make this method Async.
         private async void DownloadLink()
         {
             IDownloader downloader = new MyYoutubeDownloader();
             VideoInfo selectedVideo = await Task.Run(() => Downloader.RetrieveDownloadOptionsAsync(YoutubeUrl).Result.ToList()[SelectedVideoIndex]);
+            //await Task.Run((() => downloader.DownloadAsync(selectedVideo)));
             downloader.DownloadAsync(selectedVideo);
         }
 
@@ -58,11 +64,17 @@ namespace UserInterface
         {
             SelectedVideoIndex = CbVideoQualities.SelectedIndex;
             LvDownloads.Items.Add(VideoWithQualities[SelectedVideoIndex]);
+            //LvDownloads.Items.Add(VideosInListView.ElementAt(SelectedVideoIndex)); TODO: CHANGE COLLECTION TYPE OR CREATE A PERSONALIZATED OBJECT.
             DownloadLink();
         }
 
+        private void btDownloadUrl_Click(object sender, RoutedEventArgs e)
+        {
+            PopulateQuialitySelectorComboBox();
+        }
+
         /// TODO: perform a good condition to youtube Url via RegEx.
-        private void Window_GotFocus(object sender, RoutedEventArgs e)
+        private void tbYoutubeUrlToDownload_GotFocus(object sender, RoutedEventArgs e)
         {
             string actualClipboardText = Clipboard.GetText();
             if (actualClipboardText.Contains("youtube"))
@@ -70,11 +82,6 @@ namespace UserInterface
                 tbYoutubeUrlToDownload.Text = actualClipboardText;
                 PopulateQuialitySelectorComboBox();
             }
-        }
-
-        private void btDownloadUrl_Click(object sender, RoutedEventArgs e)
-        {
-            PopulateQuialitySelectorComboBox();
         }
     }
 }
---
+++ YouTubeDownloader/Infrastructure/DownloadItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeDownloader.Infrastructure
+{
+    public class DownloadItem : INotifyPropertyChanged
+    {
+        private double _percent;
+        public double Percent
+        {
+            get { return _percent; }
+            set
+            {
+                if (_percent.Equals(value))
+                    return;
+
+                _percent = value;
+                OnPropertyChanged("Percent");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
